d4098e7ffacee52fef78706419d1eaa4a955d98e
Jasper Seinhorst <jasper.seinhorst@icloud.com>
2023-03-03 20:00:38 +0100

    feat: add support for doors and windows

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # Homebridge plugin Porsche Taycan
-This Homebridge plugin exposes charging and battery state of your Porsche Taycan to your Home app. With this plugin you can see the current SoC of your battery and current charge state.
+This Homebridge plugin exposes different states about your vehicle(s). Such as charging state, battery level and security information about your doors and windows. 
 
 ## Supported cars:
 - Taycan (all years)
 - Taycan Sport Turismo (all years)
 - Taycan Cross Turismo (all years)
+- Hybrid Porsches with Porsche Connect capabilities (Limited support)
 
 ## Installation
-To install Homebridge Porsche Taycan:
+To install Homebridge Porsche Taycan follow these steps:
 
-- Follow the instructions on the Homebridge Wiki to install Node.js and Homebridge;
-- Install the Homebridge RPi plugin through Homebridge Config UI X or manually by:
+- Follow the instructions on the [Homebridge Wiki](https://homebridge.io/how-to-install-homebridge) to install Node.js and Homebridge;
+- Install the Homebridge Porsche Tycan plugin through Homebridge Config UI X or manually by:
   ```
   $ sudo npm -g i homebridge-porsche-taycan
   ```
 - Edit config.json and add the Porsche Taycan platform. For example like:
     ```
     {
-        "username": "<< Porsche Connect username / email >>",
-        "password": "<< Porsche Connect password >>",
+        "username": "<<Porsche Connect username>>",
+        "password": "<<Porsche Connect password>>",
         "pollInterval": 5,
         "platform": "PorscheTaycan"
     }
     ```
 
 ## Credits
-Thanks to the author of [Node Porsche Connect](https://github.com/martijndierckx/node-porsche-connect), and also everyone from [Taycan Forum](https://www.taycanforum.com/) who gave me the inspiration to start this plugin.
+Thanks to the author of [Node Porsche Connect](https://github.com/martijndierckx/node-porsche-connect) for the inspiration, and also everyone from [Taycan Forum](https://www.taycanforum.com/) who gave me the inspiration to start this plugin.
 
-This plugin started as a hobby project, but the plugin is now available to the public. Pull requests to improve the plugins are welcome. 
+This plugin started as a hobby project, but is now available to the public. Pull requests to improve the plugins are welcome. 
--- src/platform.ts
+++ src/platform.ts
@@ -2,6 +2,7 @@ import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, 
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { PorscheChargerAccessory } from './chargerAccessory';
+import { PorscheSecurityAccessory } from './securityAccessory';
 import PorscheConnect, { EngineType } from './porsche-connect';
 
 export class PorscheTaycanPlatform implements DynamicPlatformPlugin {
@@ -30,21 +31,36 @@ export class PorscheTaycanPlatform implements DynamicPlatformPlugin {
   async discoverDevices() {
     const vehicles = await this.PorscheConnectAuth.getVehicles();
     for (const vehicle of vehicles) {
+      // charger accessory
       if ([EngineType.BatteryPowered, EngineType.PluginHybrid].includes(vehicle.engineType)) {
-        const uuid = this.api.hap.uuid.generate(vehicle.vin);
-        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-        if (existingAccessory) {
-          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-          new PorscheChargerAccessory(this, existingAccessory);
+        const uuidCharger = this.api.hap.uuid.generate(`${vehicle.vin}-charger`);
+        const existingAccessoryCharger = this.accessories.find(accessory => accessory.UUID === uuidCharger);
+        if (existingAccessoryCharger) {
+          this.log.info('Restoring existing accessory from cache:', existingAccessoryCharger.displayName);
+          new PorscheChargerAccessory(this, existingAccessoryCharger);
         } else {
           this.log.info('Adding new accessory:', vehicle.modelDescription);
-          const accessory = new this.api.platformAccessory(`${vehicle.modelDescription} charger`, uuid);
+          const accessory = new this.api.platformAccessory(`${vehicle.modelDescription} charger`, uuidCharger);
           accessory.context.device = vehicle;
           new PorscheChargerAccessory(this, accessory);
           this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
         }
       } else {
         this.log.info('Vehicle not supported', vehicle.engineType);
+      }
+
+      // security accessory
+      const uuidSecurity = this.api.hap.uuid.generate(`${vehicle.vin}-security`);
+      const existingAccessorySecurity = this.accessories.find(accessory => accessory.UUID === uuidSecurity);
+      if (existingAccessorySecurity) {
+        this.log.info('Restoring existing accessory from cache:', existingAccessorySecurity.displayName);
+        new PorscheSecurityAccessory(this, existingAccessorySecurity);
+      } else {
+        this.log.info('Adding new accessory:', vehicle.modelDescription);
+        const accessory = new this.api.platformAccessory(`${vehicle.modelDescription} `, uuidSecurity);
+        accessory.context.device = vehicle;
+        new PorscheSecurityAccessory(this, accessory);
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
   }
