a2f6d5d00df21fc5004a59fc4196f789cd563632
Jasper Seinhorst <jasper.seinhorst@icloud.com>
2023-05-03 15:16:10 +0200

    chore: add basic config validation

--- src/Platform.ts
+++ src/Platform.ts
@@ -7,7 +7,7 @@ export class PorscheTaycanPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
   public readonly accessories: PlatformAccessory[] = [];
-  public readonly PorscheConnectAuth = new PorscheConnect({ username: this.config.username, password: this.config.password });
+  public PorscheConnectAuth: PorscheConnect | undefined;
   private platformVehicles: PlatformVehicle[] = [];
   private heartBeatInterval: number;
 
@@ -22,67 +22,83 @@ export class PorscheTaycanPlatform implements DynamicPlatformPlugin {
     this.accessories.push(accessory);
   }
 
-  private async initialise() {
-    await this.discoverVehicles();
+  private validateConfig(): boolean {
+    return this.config.username && this.config.password;
+  }
 
-    this.heartBeat();
-    setInterval(() => {
+  private async initialise() {
+    if (!this.validateConfig()) {
+      this.log.warn('Please specify your Porsche Connect username and password in your config file');
+      return;
+    }
+
+    try {
+      this.PorscheConnectAuth = new PorscheConnect({ username: this.config.username, password: this.config.password });
+      await this.discoverVehicles();
+
       this.heartBeat();
-    }, this.heartBeatInterval);
+      setInterval(() => {
+        this.heartBeat();
+      }, this.heartBeatInterval);
+    } catch(error) {
+      this.log.error('Porsche Connect connection failed');
+    }
   }
 
   private async discoverVehicles() {
-    const vehicles = await this.PorscheConnectAuth.getVehicles();
-    for (const vehicle of vehicles) {
-      if (vehicle.engineType === EngineType.BatteryPowered) {
-        const platformVehicle: PlatformVehicle = { vehicle, accessories: [] };
+    if (this.PorscheConnectAuth) {
+      const vehicles = await this.PorscheConnectAuth.getVehicles();
+      for (const vehicle of vehicles) {
+        if (vehicle.engineType === EngineType.BatteryPowered) {
+          const platformVehicle: PlatformVehicle = { vehicle, accessories: [] };
 
-        // Register Charger
-        const chargerUuid = this.api.hap.uuid.generate(`${vehicle.vin}-charger`);
-        const chargerExistingAccessory = this.accessories.find(accessory => accessory.UUID === chargerUuid);
+          // Register Charger
+          const chargerUuid = this.api.hap.uuid.generate(`${vehicle.vin}-charger`);
+          const chargerExistingAccessory = this.accessories.find(accessory => accessory.UUID === chargerUuid);
 
-        if (chargerExistingAccessory) {
-          platformVehicle.accessories.push(new Charger(this.config, this.log, this.api, chargerExistingAccessory));
+          if (chargerExistingAccessory) {
+            platformVehicle.accessories.push(new Charger(this.config, this.log, this.api, chargerExistingAccessory));
+          } else {
+            this.log.info('Charger added as accessory');
+            const accessory = new this.api.platformAccessory('Charger', chargerUuid);
+            accessory.context.device = vehicle;
+            platformVehicle.accessories.push(new Charger(this.config, this.log, this.api, accessory));
+            this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
+          }
+
+          // Register DirectCharge
+          const directChargeUuid = this.api.hap.uuid.generate(`${vehicle.vin}-direct-charge`);
+          const directChargeExistingAccessory = this.accessories.find(accessory => accessory.UUID === directChargeUuid);
+
+          if (directChargeExistingAccessory) {
+            platformVehicle.accessories.push(new DirectCharge(this.config, this.log, this.api, directChargeExistingAccessory));
+          } else {
+            this.log.info('Direct Charge added as accessory');
+            const accessory = new this.api.platformAccessory('Direct Charge', directChargeUuid);
+            accessory.context.device = vehicle;
+            platformVehicle.accessories.push(new DirectCharge(this.config, this.log, this.api, accessory));
+            this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
+          }
+
+          // Register DirectClimatisation
+          const directClimatisationUuid = this.api.hap.uuid.generate(`${vehicle.vin}-direct-climatisation`);
+          const directClimatisationExistingAccessory = this.accessories.find(accessory => accessory.UUID === directClimatisationUuid);
+
+          if (directClimatisationExistingAccessory) {
+            platformVehicle.accessories.push(new DirectClimatisation(this.config, this.log, this.api, directClimatisationExistingAccessory));
+          } else {
+            this.log.info('Direct Climatisation added as accessory');
+            const accessory = new this.api.platformAccessory('Direct Climatisation', directClimatisationUuid);
+            accessory.context.device = vehicle;
+            platformVehicle.accessories.push(new DirectClimatisation(this.config, this.log, this.api, accessory));
+            this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
+          }
+
+          // Register car for heart beat
+          this.platformVehicles.push(platformVehicle);
         } else {
-          this.log.info('Charger added as accessory');
-          const accessory = new this.api.platformAccessory('Charger', chargerUuid);
-          accessory.context.device = vehicle;
-          platformVehicle.accessories.push(new Charger(this.config, this.log, this.api, accessory));
-          this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
+          this.log.info(`Your Porsche ${vehicle.modelDescription} is not supported, it has an unsupported engine type ${vehicle.engineType}`);
         }
-
-        // Register DirectCharge
-        const directChargeUuid = this.api.hap.uuid.generate(`${vehicle.vin}-direct-charge`);
-        const directChargeExistingAccessory = this.accessories.find(accessory => accessory.UUID === directChargeUuid);
-
-        if (directChargeExistingAccessory) {
-          platformVehicle.accessories.push(new DirectCharge(this.config, this.log, this.api, directChargeExistingAccessory));
-        } else {
-          this.log.info('Direct Charge added as accessory');
-          const accessory = new this.api.platformAccessory('Direct Charge', directChargeUuid);
-          accessory.context.device = vehicle;
-          platformVehicle.accessories.push(new DirectCharge(this.config, this.log, this.api, accessory));
-          this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
-        }
-
-        // Register DirectClimatisation
-        const directClimatisationUuid = this.api.hap.uuid.generate(`${vehicle.vin}-direct-climatisation`);
-        const directClimatisationExistingAccessory = this.accessories.find(accessory => accessory.UUID === directClimatisationUuid);
-
-        if (directClimatisationExistingAccessory) {
-          platformVehicle.accessories.push(new DirectClimatisation(this.config, this.log, this.api, directClimatisationExistingAccessory));
-        } else {
-          this.log.info('Direct Climatisation added as accessory');
-          const accessory = new this.api.platformAccessory('Direct Climatisation', directClimatisationUuid);
-          accessory.context.device = vehicle;
-          platformVehicle.accessories.push(new DirectClimatisation(this.config, this.log, this.api, accessory));
-          this.api.registerPlatformAccessories('homebridge-porsche-taycan', 'PorscheTaycan', [accessory]);
-        }
-
-        // Register car for heart beat
-        this.platformVehicles.push(platformVehicle);
-      } else {
-        this.log.info(`Your Porsche ${vehicle.modelDescription} is not supported, it has an unsupported engine type ${vehicle.engineType}`);
       }
     }
   }
